4775f1f2e2ae16d44c36e4d5607f2bf31e3e47fb
SantatraM <santatryn@gmail.com>
2023-11-21 17:33:49 +0300

    commit

---
+++ basketball/script/dropview.sql
@@ -0,0 +1,8 @@
+drop view v_joueursRPM;
+drop view v_joueursPPM;
+drop view v_joueursPDPM;
+drop view v_joueursMPM;
+drop view v_joueursLF;
+drop view v_joueursFG;
+drop view v_joueursEFF;
+drop view v_joueurs3P;
