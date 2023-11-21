85f42930b6255ec984f1dbb483f82274a2d852d7
Henintsoa <118676248+ETU001778-W31-P15A-Henintsoa@users.noreply.github.com>
2023-11-21 10:36:09 +0300

    commit

---
+++ basketball/src/test/java/basketball/basketball/BasketballApplicationTests.java
@@ -0,0 +1,13 @@
+package basketball.basketball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
